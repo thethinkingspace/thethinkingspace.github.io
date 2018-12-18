37cd68140b2af09cc3ccc85962e14125302a50a2
Ting Kuei <ting@kuei.com>
2018-12-18 21:39:28 +0000

    Static site

--- components/Parallax.js
+++
@@ -1,9 +0,0 @@
-export default ({ children }) => (
-  <div className="parallax-section parallax1">
-    <div className="grid grid-pad">
-      <div className="col-1-1">
-        <div className="content content-header">{children}</div>
-      </div>
-    </div>
-  </div>
-);
