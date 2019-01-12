dd119937d1aa784948ec7642ae284c34c4d7ffae
Thinking Space <43249074+thethinkingspace@users.noreply.github.com>
2019-01-12 21:50:59 +0000

    Update tweets.js

--- js/tweets.js
+++ js/tweets.js
@@ -8,6 +8,6 @@ $(document).ready(function($) {
     date_format: "style2",
     visible_tweets: 2,
     request_url:
-      "https://script.google.com/macros/s/AKfycbxqYNkNejlaS6zL1PJ6Ud9Yq7zKc9zByDKGYeMuNgNIXxmLXJU/exec"
+      "https://script.google.com/macros/s/AKfycbyB9dRPsQHInczGUhevAtZXBPa-vvcIIVunWsgMbKWnhpVbbgg/exec"
   });
 });
